7ea0ac01644bd4c61ef4d0195672f9d67b5352f2
MercyNJ <mercinjuguna@gmail.com>
2023-09-25 03:45:24 -0700

    Gathered data fron an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,38 +7,27 @@ Displays an employess's todo list's progress.
 import requests
 import sys
 
+if __name__ == '__main__':
+    empl_id = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    empl_url = baseUrl + "/" + empl_id
 
-def get_employee_todo_progress(employee_id):
-    """ Display empl todo progress."""
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    response = requests.get(empl_url)
+    empl_name = response.json().get('name')
 
-    try:
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-        employee_name = employee_data.get("name")
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
+    todoUrl = empl_url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    count_complete = 0
+    complete_tasks = []
 
-        total_tasks = len(todos_data)
-        completed_tasks = sum(
-                1 for task in todos_data if task["completed"])
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, completed_tasks, total_tasks))
-        for task in todos_data:
-            if task["completed"]:
-                print("\t{}".format(task['title']))
+    for task in tasks:
+        if task.get('completed'):
+            complete_tasks.append(task)
+            count_complete += 1
 
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
-    except KeyError:
-        print("Employee not found.")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(empl_name, count_complete, len(tasks)))
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
+    for task in complete_tasks:
+        print("\t {}".format(task.get('title')))
