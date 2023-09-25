e4bcffb0fe8668595db09911c79f3146b5644f94
MercyNJ <mercinjuguna@gmail.com>
2023-09-25 04:32:22 -0700

    Export data in csv format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,38 +6,21 @@ Displays an employess's todo list's progress.
 
 import requests
 import sys
-import csv
 
 if __name__ == '__main__':
     empl_id = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
     empl_url = baseUrl + "/" + empl_id
     response = requests.get(empl_url)
-    empl_name = response.json().get('name')
+    empl_username = response.json().get('username')
 
     todoUrl = empl_url + "/todos"
     response = requests.get(todoUrl)
 
     tasks = response.json()
-    count_complete = 0
-    complete_tasks = []
 
-    for task in tasks:
-        if task.get('completed'):
-            complete_tasks.append(task)
-            count_complete += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(empl_name, count_complete, len(tasks)))
-
-    for task in complete_tasks:
-        print("\t {}".format(task.get('title')))
-
-    csv_filename = "{}.csv".format(empl_id)
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    with open('{}.csv'.format(empl_id), 'w') as file:
         for task in tasks:
-            csv_writer.writerow([empl_id, empl_name, task.get(
-                'completed'), task.get('title')])
+            file.write('"{}","{}","{}","{}"\n'.format(
+                empl_id, empl_username, task.get(
+                    'completed'), task.get('title')))
